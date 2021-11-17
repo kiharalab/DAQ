c9752b23c79fd19137f6c9b256b5d1ad8af92d42
xiaowang <mymac@xiaowang.local>
2021-11-18 05:22:11 +0800

    fix the args info

--- ops/argparser.py
+++ ops/argparser.py
@@ -13,9 +13,9 @@ def argparser():
     parser.add_argument("--stride",type=int,default=1,help="Stride size for scanning maps (default:1)")
     parser.add_argument("--voxel_size",type=int,default=11,help="Input voxel size (default:11)")
     parser.add_argument("--gpu",type=str,default=None,help="specify the gpu we to use")
-    parser.add_argument('--batch_size', type=int, default=256, help='batch size for inference')
+    parser.add_argument('--batch_size', type=int, default=256, help='batch size for inference (default:256)')
     parser.add_argument('--cardinality', default=32, type=int, help='ResNeXt cardinality')
-    parser.add_argument("--window",default=9,type=int,help="hal window size to smooth the score for output (default:9)")
+    parser.add_argument("--window",default=9,type=int,help="half window size to smooth the score for output (default:9)")
     args = parser.parse_args()
     params = vars(args)
     return params
